70df63e42552ec5c6c0174107f27aaafcbf6a907
Carsten Fortmann-Grote <carsten.fortmann-grote@evolbio.mpg.de>
2019-05-15 12:29:25 +0200

    Bind to 0.0.0.0

--- server.js
+++ server.js
@@ -127,6 +127,5 @@ app.get('*', function (req, res){
     });
 });
 
-app.listen(port, () => {
-    console.log(`ISA-explorer listening on port ${port}`);
-});
+app.listen(port, "0.0.0.0");
+console.log(`ISA-explorer listening on port ${port}`);
